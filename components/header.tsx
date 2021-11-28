139b56ff227bdc5731006365dc33c953407c0914
Chan Rose <chanrose@pm.me>
2021-11-28 07:32:39 +0700

    update header

--- components/header.tsx
+++ components/header.tsx
@@ -63,7 +63,7 @@ export const Header = ({ data }) => {
 
   const router = useRouter();
 
-    return (
+  return (
     <div>
       <Container className={`relative z-10 py-0 max-w-8xl dark:bg-wgray-800`}>
         <div className="flex items-center justify-between">
@@ -83,32 +83,25 @@ export const Header = ({ data }) => {
           </h4>
           <div className="justify-between hidden space-x-2 md:flex">
             <Link href="/">
-            <a
-              className={`text-opacity-90 text-wgray-600 group bg-white hover:bg-${theme["color"]}-600 hover:text-${theme["color"]}-50 px-3 py-2 rounded-md items-center text-base font-medium hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
-            >
-              Home
-            </a>
+              <a
+                className={`text-opacity-90 trasition duration-200 ease-in-out text-wgray-600 group bg-white hover:text-wgray-200 px-3 py-2 hover:bg-wgray-500 rounded-md items-center text-base font-medium hover:text-opacity-100`}
+              >
+                Home
+              </a>
             </Link>
             <Popover className="relative">
               {({ open }) => (
                 <>
                   <Popover.Button
-                    className={`
-                ${open ? "" : "text-opacity-90"}
-                text-${theme["color"]}-600 group hover:bg-${
-                      theme["color"]
-                    }-600 hover:text-${theme["color"]}-50 focus:bg-${
-                      open && theme["color"]
-                    }-700 focus:text-${
-                      open && theme["color"]
-                    }-50 px-3 py-2 rounded-md inline-flex items-center text-base font-medium hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+                    className={`inline-flex text-opacity-90 trasition duration-200 ease-in-out text-wgray-600 group bg-white hover:text-wgray-200 px-3 py-2 hover:bg-wgray-500 rounded-md items-center text-base font-medium hover:text-opacity-100 ${
+                      open && "bg-wgray-400 text-wgray-100"
+                    }`}
                   >
                     <span>Posts</span>
+
                     <ChevronDownIcon
                       className={`${open ? "" : "text-opacity-70"}
-                  ml-2 h-5 w-5 text-${
-                    theme["color"]
-                  }-300 group-hover:text-opacity-80 transition ease-in-out duration-150`}
+                  ml-2 h-5 w-5 text-wgray-300 group-hover:text-opacity-80 transition ease-in-out duration-150`}
                       aria-hidden="true"
                     />
                   </Popover.Button>
@@ -138,9 +131,7 @@ export const Header = ({ data }) => {
                         </div>
                         <div className="p-4 bg-gray-50">
                           <Link href="/posts">
-                            <a
-                              className="px-2 py-2 flow-root transition duration-150 ease-in-out rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
-                            >
+                            <a className="px-2 py-2 flow-root transition duration-150 ease-in-out rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
                               <span className="flex items-center">
                                 <span className="text-sm font-medium text-gray-900">
                                   All Posts
@@ -160,15 +151,9 @@ export const Header = ({ data }) => {
               {({ open }) => (
                 <>
                   <Popover.Button
-                    className={`
-                ${open ? "" : "text-opacity-90"}
-                text-${theme["color"]}-600 group hover:bg-${
-                      theme["color"]
-                    }-600 hover:text-${theme["color"]}-50 focus:bg-${
-                      open && theme["color"]
-                    }-700 focus:text-${
-                      open && theme["color"]
-                    }-50 px-3 py-2 rounded-md inline-flex items-center text-base font-medium hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+                    className={`inline-flex text-opacity-90 trasition duration-200 ease-in-out text-wgray-600 group bg-white hover:text-wgray-200 px-3 py-2 hover:bg-wgray-500 rounded-md items-center text-base font-medium hover:text-opacity-100 ${
+                      open && "bg-wgray-400 text-wgray-100"
+                    }`}
                   >
                     <span>About</span>
                     <ChevronDownIcon
@@ -204,12 +189,13 @@ export const Header = ({ data }) => {
                           ))}
                         </div>
                         <div className="p-4 bg-gray-50">
-                          <a href={data.email} 
+                          <a
+                            href={data.email}
                             className="px-2 py-2 flow-root transition duration-150 ease-in-out rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
-                            target="_blank">
+                            target="_blank"
+                          >
                             Email
                           </a>
-                          
                         </div>
                       </div>
                     </Popover.Panel>
@@ -242,13 +228,7 @@ export const Header = ({ data }) => {
                   <Popover.Button
                     className={`
                 ${open ? "" : "text-opacity-90"}
-                text-${theme["color"]}-600 group hover:bg-${
-                      theme["color"]
-                    }-600 hover:text-${theme["color"]}-50 focus:bg-${
-                      open && theme["color"]
-                    }-700 focus:text-${
-                      open && theme["color"]
-                    }-50 px-3 py-2 rounded-md inline-flex items-center text-base font-medium hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+                text-wgray-600 group hover:bg-wgray-600 hover:text-wgray-50 focus:bg-wgray-700 focus:text-wgray-50 px-3 py-2 rounded-md inline-flex items-center text-base font-medium hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
                   >
                     <span>
                       <MenuAlt3Icon className={`h-6 w-6`} />
@@ -282,41 +262,40 @@ export const Header = ({ data }) => {
                             </div>
 
                             <div className="-mr-2">
-                              <Popover.Button className="inline-flex items-center justify-center p-2 rounded-md text-wgray-400 dark:text-cgray-50 hover:text-wgray-500 dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-rose-200">
+                              <Popover.Button className="inline-flex items-center justify-center p-2 rounded-md text-wgray-400 dark:text-cgray-50 hover:text-wgray-500 dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-wgray-200">
                                 <span className="sr-only">Close menu</span>
                                 <XIcon className="w-6 h-6" aria-hidden="true" />
                               </Popover.Button>
                             </div>
                           </div>
-                       <nav className="grid gap-y-4">
-                          {data.posts.map((item) => (
-                            <Link href={item.href} key={item.label}>
-                                  <a className="text-sm font-medium text-gray-900">
-                                    {item.label}
-                                  </a>
-                            </Link>
-                          ))}
-                       </nav>
+                          <nav className="grid gap-y-4">
+                            {data.posts.map((item) => (
+                              <Link href={item.href} key={item.label}>
+                                <a className="text-sm font-medium text-gray-900">
+                                  {item.label}
+                                </a>
+                              </Link>
+                            ))}
+                          </nav>
                         </div>
                         <div className="p-4 bg-gray-50">
                           <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                           <Link href="/posts">
-                            <a>
-                              <span className="flex items-center">
-                                <span className="text-sm font-medium text-gray-900">
-                                  All Posts
+                            <Link href="/posts">
+                              <a>
+                                <span className="flex items-center">
+                                  <span className="text-sm font-medium text-gray-900">
+                                    All Posts
+                                  </span>
                                 </span>
-                              </span>
-                            </a>
-                           </Link>
-                          {data.nav.map((item) => (
-                            <Link href={item.href} key={item.label}>
-                                  <a className="text-sm font-medium text-gray-900">
-                                    {item.label}
-                                  </a>
+                              </a>
                             </Link>
-                          ))}
- 
+                            {data.nav.map((item) => (
+                              <Link href={item.href} key={item.label}>
+                                <a className="text-sm font-medium text-gray-900">
+                                  {item.label}
+                                </a>
+                              </Link>
+                            ))}
                           </div>
                         </div>
                       </div>
